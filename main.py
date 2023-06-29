944fe2df93dc36549d53eafbb6b80b4bb2ddaeb2
rocco.malvaso <malvasorocco3@gmail.com>
2023-06-29 20:58:24 +0200

    Test render.com

--- main.py
+++ main.py
@@ -1,45 +1,44 @@
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes , CallbackQueryHandler, Updater
+from telegram.ext import CallbackContext, CommandHandler, MessageHandler, Updater
 import requests
 import const as api
 import json
 import os
+
 print('Mi sto avviando')
 
 TOKEN = os.environ['BOT_TOKEN']
 BOT_USERNAME = '@prontosoccorsostatobot'
 HOSTING_URL = os.environ["H_URL"]
 keyboard = [
-        [InlineKeyboardButton("🏥 San Raffaele", callback_data='SanRaffaele')],
-        [InlineKeyboardButton("🏥 Ospedale San Carlo Borromeo", callback_data='SanCarloBorromeo')],
-        [InlineKeyboardButton("🏥 Ospedale di Melzo", callback_data='OspedaleMelzo')],
-
-    ]
+    [InlineKeyboardButton("🏥 San Raffaele", callback_data='SanRaffaele')],
+    [InlineKeyboardButton("🏥 Ospedale San Carlo Borromeo", callback_data='SanCarloBorromeo')],
+    [InlineKeyboardButton("🏥 Ospedale di Melzo", callback_data='OspedaleMelzo')],
+]
 reply_markup = InlineKeyboardMarkup(keyboard)
 
-async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text('''Ciao 👋🏻 mi presento sono ProntoSoccorso Bot, con me potrai controllare in tempo reale
-lo stato dei pronto soccorso nella provincia di Milano. ⚪️🟢🟡🔴''', reply_markup = InlineKeyboardMarkup(keyboard))
+def start_command(update: Update, context: CallbackContext):
+    update.message.reply_text('''Ciao 👋🏻 mi presento sono ProntoSoccorso Bot, con me potrai controllare in tempo reale
+    lo stato dei pronto soccorso nella provincia di Milano. ⚪️🟢🟡🔴''', reply_markup=reply_markup)
 
-async def prontosoccorso(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text('''Ecco la lista dei pronto soccorsi nella zona di 
-Milano. ⚪️🟢🟡🔴''', reply_markup = InlineKeyboardMarkup(keyboard))
+def prontosoccorso(update: Update, context: CallbackContext):
+    update.message.reply_text('''Ecco la lista dei pronto soccorsi nella zona di 
+    Milano. ⚪️🟢🟡🔴''', reply_markup=reply_markup)
 
-
-async def button(update: Update, context: ContextTypes.DEFAULT_TYPE):
+def button(update: Update, context: CallbackContext):
     query = update.callback_query
-    await query.answer() 
-    contenuto = await fetch(query.data)
-    attesa = list(contenuto[3].values()) #bianchi, gialli, rossi, totale, verde
-    carico = list(contenuto[4].values()) #bianchi, gialli, rossi, totale, verde
-    await context.bot.send_message(chat_id=query.message.chat_id, text=f"""Il nome del pronto soccorso selezionato è """
-    f"""{contenuto[0]}.\nIl pronto soccorso è attualmento : {contenuto[2]}.\nI """
-    f"""pazienti in attesa sono :\n⚪️ {attesa[0]}\n🟢 {attesa[4]}\n🟡 {attesa[1]}\n🔴 {attesa[2]}\nTotale {attesa[3]}"""
-    f"""\nI pazienti presi in carico sono :\n⚪️ {carico[0]}\n🟢 {carico[4]}\n🟡 {carico[1]}\n🔴 {carico[2]}\nTotale {carico[3]}""")
-    await context.bot.send_contact(chat_id=query.message.chat_id,phone_number=contenuto[1], first_name=contenuto[0])
-    #await query.edit_message_text(text="Updated data") Per modificare il messaggio
+    query.answer()
+    contenuto = fetch(query.data)
+    attesa = list(contenuto[3].values())  # bianchi, gialli, rossi, totale, verde
+    carico = list(contenuto[4].values())  # bianchi, gialli, rossi, totale, verde
+    query.message.reply_text(f"""Il nome del pronto soccorso selezionato è """
+                             f"""{contenuto[0]}.\nIl pronto soccorso è attualmento : {contenuto[2]}.\nI """
+                             f"""pazienti in attesa sono :\n⚪️ {attesa[0]}\n🟢 {attesa[4]}\n🟡 {attesa[1]}\n🔴 {attesa[2]}\nTotale {attesa[3]}"""
+                             f"""\nI pazienti presi in carico sono :\n⚪️ {carico[0]}\n🟢 {carico[4]}\n🟡 {carico[1]}\n🔴 {carico[2]}\nTotale {carico[3]}""")
+    query.message.reply_contact(phone_number=contenuto[1], first_name=contenuto[0])
+    # query.edit_message_text(text="Updated data") Per modificare il messaggio
 
-async def fetch(ospedale):
+def fetch(ospedale):
     response = requests.get(api.API_URL[ospedale])
     data = json.loads(response.text)
     nomePS = data[0]["anagraficaPS"]["struttura"]["denominazione"]
@@ -51,17 +50,20 @@ async def fetch(ospedale):
         apertura = "Chiuso"
     pazientiAttesa = data[0]["statoPS"]["numPazientiInAttesa"]
     pazientiCarico = data[0]["statoPS"]["numPazientiInCarico"]
-    return nomePS, telefono, apertura , pazientiAttesa , pazientiCarico
+    return nomePS, telefono, apertura, pazientiAttesa, pazientiCarico
 
-async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
+def error(update: Update, context: CallbackContext):
     print(f'Update {update} caused error {context.error}')
 
 if __name__ == '__main__':
-    app = Application.builder().token(TOKEN).build()
-    app.add_handler(CommandHandler('start', start_command))
-    app.add_handler(CommandHandler('messaggio', prontosoccorso))
-    app.add_handler(MessageHandler(filters.TEXT, button))
-    app.add_handler(CallbackQueryHandler(button))
-    app.add_error_handler(error)
+    updater = Updater(TOKEN)
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler('start', start_command))
+    dispatcher.add_handler(CommandHandler('messaggio', prontosoccorso))
+    dispatcher.add_handler(MessageHandler(Filters.text, button))
+
+    dispatcher.add_error_handler(error)
+
     print('Ascolto...')
-    Updater.start_webhook(listen="0.0.0.0", webhook_url=f'{HOSTING_URL}/{TOKEN}', url_path=TOKEN, port=int(os.environ.get('PORT', 5000)))
+    updater.start_polling()
